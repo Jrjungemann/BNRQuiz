623d9e935972d5e57229a7ec1bfb94aa21f1d1b8
Jonathan Jungemann <jrjungemann@gmail.com>
2015-06-15 19:18:44 -0500

    Initial Commit

---
+++ Quiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quiz
+//
+//  Created by Jon Jungemann on 6/15/15.
+//  Copyright (c) 2015 BNF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
